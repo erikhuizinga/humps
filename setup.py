1c8fda4ef2c5e554a91b3fb6d6b5c5d4373bf94f
Erik Huizinga <erik.huizinga@nedap.com>
2018-03-13 08:55:09 +0100

    Extend long package description to match README.rst

--- setup.py
+++ setup.py
@@ -10,15 +10,36 @@ setup(
     url="https://github.com/erikhuizinga/humps",
     description="camelCase converter",
     long_description=
-    "*********\n"
-    "``humps``\n"
-    "*********\n"
-    "\n"
-    "*Convert any string to camelCase.*\n"
-    "\n"
-    "camelCase starts with a lower case alphabetic character, the rest of the string contains alphanumeric "
-    "characters. Any character case in the input is ignored. Any spaces in the input capitalise the following "
-    "character if alphabetic, except for the first character. Any non-alphanumeric characters are ignored.",
+    """*********
+``humps``
+*********
+
+*Convert any string to camelCase.*
+
+Description
+===========
+
+camelCase starts with a lower case alphabetic character, the rest of the string contains alphanumeric characters. Any character case in the input is ignored. Any spaces in the input capitalise the following character if alphabetic, except for the first character. Any non-alphanumeric characters are ignored.
+
+Installation
+============
+
+Shell:
+
+.. code-block:: sh
+
+  pip install humps
+
+
+Python:
+
+.. code-block:: python
+
+  from humps.camel import case
+
+  print(case('Hello, World!'))
+  # Output: helloWorld
+""",
     keywords=["camelcase", 'camel', 'case', "string", "conversion"],
     classifiers=[
         "Development Status :: 4 - Beta",
